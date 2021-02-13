8ee1e6a523b457e61c5023c1f3895d4e9334b8bf
monica <57308517+mlewis55@users.noreply.github.com>
2021-02-13 09:14:09 -0500

    added library

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button } from 'react-native-ui-lib';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
