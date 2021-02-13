4decbae77d0aa75396291431bc39d0b9a3efb5f5
“Natalie <umanzor2949@gmail.com>
2021-02-13 03:51:08 -0500

    added nacho ui and countdown libraries, added files for individual components

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button } from 'react-native-ui-lib';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+const Pulse = require('react-native-pulse').default;
+
+const Spacer = ({ ...styles }) => (
+  <View style={styles} />
+)
 
 const HomeScreen = ({ navigation }) => (
+
   <View>
     <Text>Home Screen</Text>
+    <Spacer height={ 20 } />
     <Button
       title="Go to Grounding"
       onPress={() => navigation.navigate('Grounding')}
     />
+    <Spacer height={20} />
     <Button
       title="Go to Deep Breathing"
       onPress={() => navigation.navigate('Deep Breathing')}
     />
+    <Spacer height={20} />
+    
     <Button
-      title="Go to Resources"
+      label="Go to Resources"
+      style={{marginBottom: 20}}
+      size={Button.sizes.medium}
       onPress={() => navigation.navigate('Resources')}
     />
   </View>
@@ -31,13 +43,26 @@ const GroundingScreen = ({ navigation }) => {
 
 const DeepBreathingScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Deep Breathing Screen</Text>
+    <View style={{ 
+      flexDirection:'column',
+      justifyContent:'center',
+      flex: 1,
+    }}>
+      <Text style={{
+        width: "100%",
+        textAlign: "center",
+      }}>
+        Deep Breathing Screen
+      </Text>
+      <Pulse color="blue" numPulses={ 1 } diameter={ 400 } speed={ 40 } duration={ 14000 } />
     </View>
   )
 }
 
 const ResourcesScreen = ({ navigation }) => {
+  /*
+
+  */
   return (
     <View>
       <Text>Resources Screen</Text>
---
+++ Grounding.js
@@ -0,0 +1,9 @@
+// step 1) click the grounding option
+// step 2) instructions screen
+// step 3) user picks the category
+
+
+
+// top 75% of screen is an image
+// ask the user how many items are in the picture
+// store the answers in hooks/redux/database
